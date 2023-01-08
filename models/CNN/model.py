37a44532ac9619e95a3c903b8ca513b2f84577c6
Rima Al-Ghossein <ralghoss-21@lame15.enst.fr>
2023-01-08 21:57:37 +0100

    fix: CNN classifier

--- models/CNN/model.py
+++ models/CNN/model.py
@@ -7,7 +7,10 @@ class CNN(torch.nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.resnet = resnet152(pretrained= True)
-        self.resnet.fc = nn.Sequential(nn.Linear(8192, 64), nn.Linear(64, 64),nn.Linear(64, 2))
+        self.resnet.fc = nn.Sequential(
+          nn.Linear(self.resnet.fc.in_features, 64),
+          nn.Linear(64, 64),
+          nn.Linear(64, 2))
 
 
     def forward(self, batch_data):
